ccd15194c4868e0ddae33cfd3b7dedfe63c6ad03
Kleyton João <35746510+kleyton032@users.noreply.github.com>
2018-03-04 01:01:34 -0300

    finalizando pesquisa cliente

--- src/main/java/com/sistema/cervejaria/model/TipoPessoa.java
+++ src/main/java/com/sistema/cervejaria/model/TipoPessoa.java
@@ -5,8 +5,20 @@ import com.sistema.cervejaria.model.validation.group.CpfGroup;
 
 public enum TipoPessoa {
 
-	FISICA("Física", "CPF", "000.000.000-00", CpfGroup.class), 
-	JURIDICA("Jurídica", "CNPJ", "00.000.000/0000-00", CnpjGroup.class);
+	FISICA("Física", "CPF", "000.000.000-00", CpfGroup.class) {
+		@Override
+		public String formatar(String cpfOuCnpj) {
+
+			return cpfOuCnpj.replaceAll("(\\d{3})(\\d{3})(\\d{3})", "$1.$2.$3-");
+		}
+	},
+	JURIDICA("Jurídica", "CNPJ", "00.000.000/0000-00", CnpjGroup.class) {
+		@Override
+		public String formatar(String cpfOuCnpj) {
+
+			return cpfOuCnpj.replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})", "$1.$2.$3/$4-");
+		}
+	};
 
 	private String descricao;
 	private String documento;
@@ -17,8 +29,10 @@ public enum TipoPessoa {
 		this.descricao = descricao;
 		this.documento = documento;
 		this.mascara = mascara;
-		this.grupo = grupo; 
+		this.grupo = grupo;
 	}
+
+	public abstract String formatar(String cpfOuCnpj);
 
 	public String getDescricao() {
 		return descricao;
@@ -43,12 +57,13 @@ public enum TipoPessoa {
 	public void setMascara(String mascara) {
 		this.mascara = mascara;
 	}
+
 	public Class<?> getGrupo() {
 		return grupo;
 	}
-	
+
 	public static String removerFormtatacaoCpfOuCnpj(String cpfOuCnpj) {
-		
+
 		return cpfOuCnpj.replaceAll("\\.|-|/", "");
 	}
 
